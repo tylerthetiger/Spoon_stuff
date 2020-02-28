ed75fec018a77f5b342ad63979703aed43713be7
tyler <tyleroar@gmail.com>
2020-02-27 20:41:42 -0500

    interpreter working for hello world and happy birthday examples provided

--- spoon.py
+++ spoon.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2
+import sys
+
 class Instruction:
   def __init__(self,instructionStr,handler):
     self.instructionStr = instructionStr
@@ -31,13 +33,9 @@ class Spoon:
     
     inst = Instruction(symbol0*2+symbol1+symbol0*2,self.whileStart)
     self.instructionList.append(inst)
-    inst = Instruction(symbol0*2+symbol1*2,self.whileEnd)
-    self.instructionList.append(inst)
-  
-  #  inst = Instruction()
-   # i = 
-    #self.instructionMapping = dict()
-    #self.instructionMapping['']
+    #we don't actually look for the while end here
+  #  inst = Instruction(symbol0*2+symbol1*2,self.whileEnd)
+  #  self.instructionList.append(inst)
   def executeOneInstruction(self):
     #make sure our mem array is large enough to be referenced
     while(len(self.a)<=self.ptr):
@@ -45,49 +43,62 @@ class Spoon:
     for i in self.instructionList:
       if self.code[self.PC:self.PC+len(i.instructionStr)] == i.instructionStr:
         return i.handler()
+    self.code = self.code.replace("_","0")
+    self.code = self.code.replace("-","1")
     raise Exception("I dont know how to decode instruction: {}".format(self.code[self.PC:]))
-  #a[ptr]++
   def incMem(self):
-   # print "look at me, i'm the incHandler!"
     self.a[self.ptr]+=1
     self.PC+=1
-  #a[ptr]--
   def decMem(self):
     self.a[self.ptr]-=1
-  #  print "look at me, i'm the decHandler!"
     self.PC+=3
   def incPtr(self):
     self.ptr+=1
     self.PC+=3
   def decPtr(self):
-    self.ptr-=3
+    self.ptr-=1
     self.PC+=3
   def putChar(self):
     letter=self.a[self.ptr]
     letter=chr(letter)
-    print letter,
+    sys.stdout.write(letter)
+    sys.stdout.flush()
     self.PC+=6
   def getChar(self):
     char = raw_input()[0]
-    #print b
     self.a[self.ptr] = ord(char)
     self.PC+=7
+  def executeUntilLoopEnd(self):
+    loopEndInstr = self.symbol0*2+self.symbol1*2
+    while True:
+      if self.code[self.PC:self.PC+len(loopEndInstr)]==loopEndInstr:
+        newPC =  self.PC+len(loopEndInstr)
+        return newPC
+      else:
+        self.printState()
+        self.executeOneInstruction()
+
   def whileStart(self):
-    raise Exception("TODO, write while start")
-  def whileEnd(self):
-    raise Exception("TODO, write while end")
+    loopStart = self.PC+5
+    self.PC+=5
+    while (self.a[self.ptr]!=0):
+      loopEnd = self.executeUntilLoopEnd()
+      self.PC = loopStart
+    self.PC = loopEnd#jump out of the loop!
+    #loop end needs to be just after the loop end instruction
+
+ # def whileEnd(self):
+  #  raise Exception("TODO, write while end")
   def printState(self):
+    return
     print "PC: {}\na:{}\nptr:{}".format(self.PC,self.a,self.ptr)
     print "Code: {}".format(self.code[self.PC:])
   def runIt(self):
-    #a = Instruction("1001",self.incHandler)
-    #print a
-    #a.handler()
     while self.PC<len(self.code):
       self.printState()
       self.executeOneInstruction()
 
 if __name__ == "__main__":
-  j = Spoon("code.bin")
-  #j = Spoon("test.bin")
+  #j = Spoon("code.bin")
+  j = Spoon("test.bin",symbol0="_",symbol1="-")
   j.runIt()
